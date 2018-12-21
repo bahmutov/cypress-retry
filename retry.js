fbcdd8ffa55ae6434e8be3a4c139ba5cb9ec4b55
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-20 23:20:56 -0500

    print list of failed

--- retry.js
+++ retry.js
@@ -1,10 +1,28 @@
+const { skipAllOtherTests } = require('./skip')
 const cypress = require('cypress')
-cypress.run({
-  spec: 'cypress/integration/spec.js',
-  env: {
-    // make the test fail on first attempt
-    FAIL: '1'
-  }
-}).then(results => {
-  console.log(results.runs[0].tests)
-})
+const { map, prop, filter, propEq, tap, head } = require('ramda')
+const debug = require('debug')('cypress-retry')
+const debugOneLine = x => debug('%o', x)
+
+const specFilename = 'cypress/integration/spec.js'
+
+cypress
+  .run({
+    spec: specFilename,
+    env: {
+      // make the test fail on first attempt
+      FAIL: '1'
+    }
+  })
+  .then(tap(debugOneLine))
+  .then(prop('runs'))
+  .then(head)
+  .then(first => {
+    if (first.stats.failures) {
+      console.log('failures: %d', first.stats.failures)
+      const failedTests = filter(propEq('state', 'failed'), first.tests)
+      console.log(
+        'failed test titles:\n' + map(prop('title'), failedTests).join('\n')
+      )
+    }
+  })
