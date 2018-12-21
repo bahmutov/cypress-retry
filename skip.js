a8c78f16fdbe71e0a0e0e9e248023685c03dedd4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-20 23:08:40 -0500

    change it to it.skip

--- skip.js
+++ skip.js
@@ -1,11 +1,14 @@
 const falafel = require('falafel')
 const fs = require('fs')
-const source = fs.readFileSync('./cypress/integration/spec.js', 'utf8')
+const R = require('ramda')
 
-const isTestBlock = (name) => (node) => {
-  return node.type === 'CallExpression' &&
-    node.callee && node.callee.type === 'Identifier' &&
+const isTestBlock = name => node => {
+  return (
+    node.type === 'CallExpression' &&
+    node.callee &&
+    node.callee.type === 'Identifier' &&
     node.callee.name === name
+  )
 }
 
 const isDescribe = isTestBlock('describe')
@@ -14,38 +17,58 @@ const isContext = isTestBlock('context')
 
 const isIt = isTestBlock('it')
 
-const getItsName = (node) =>
-  node.arguments[0].value
+const getItsName = node => node.arguments[0].value
 
-const findSuites = (node, names = []) => {
-  if (!node) {
-    return
+const skipAllOtherTests = (specFilename, leaveTests) => {
+  const source = fs.readFileSync(specFilename, 'utf8')
+
+  const shouldLeaveTest = testName => leaveTests.some(R.equals(testName))
+
+  const findSuites = (node, names = []) => {
+    if (!node) {
+      return
+    }
+
+    if (isDescribe(node) || isContext(node)) {
+      names.push(getItsName(node))
+    }
+
+    return findSuites(node.parent, names)
   }
 
-  if (isDescribe(node) || isContext(node)) {
-    names.push(getItsName(node))
-  }
+  const skipAllTests = node => {
+    // if (isIt(node)) {
+    //   console.log('it %s', getItsName(node))
+    // } else if (isDescribe(node)) {
+    //   console.log('describe %s', getItsName(node))
+    // } else if (isContext(node)) {
+    //   console.log('context %s', getItsName(node))
+    // }
 
-  return findSuites(node.parent, names)
+    if (isIt(node)) {
+      const names = [getItsName(node)]
+      findSuites(node, names)
+
+      // we were searching from inside out, thus need to revert the names
+      const testName = names.reverse()
+      console.log('found test', testName)
+      if (!shouldLeaveTest(testName)) {
+        console.log('should skip this test')
+        node.update('it.skip' + node.source().substr(2))
+        // console.log(node)
+      }
+    }
+  }
+  const output = falafel(source, skipAllTests)
+  console.log(output)
 }
 
-const skipAllTests = (node) => {
-  // if (isIt(node)) {
-  //   console.log('it %s', getItsName(node))
-  // } else if (isDescribe(node)) {
-  //   console.log('describe %s', getItsName(node))
-  // } else if (isContext(node)) {
-  //   console.log('context %s', getItsName(node))
-  // }
-
-  if (isIt(node)) {
-    const names = [getItsName(node)]
-    findSuites(node, names)
-
-    // we were searching from inside out, thus need to revert the names
-    const testName = names.reverse()
-    console.log('found test', testName)
-  }
+module.exports = {
+  skipAllOtherTests
 }
-const output = falafel(source, skipAllTests)
-// console.log(output)
+
+if (!module.parent) {
+  skipAllOtherTests('./cypress/integration/spec.js', [
+    ['outside', 'inside', 'fails when env variable FAIL is set']
+  ])
+}
